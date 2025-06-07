4c3c68888fbac8a3960112bb7b37dcddf1ac6a16
ernes <ernechelsea@gmail.com>
2025-06-06 22:25:00 -0400

    Projecto Base Listo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Home</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 class="header">Home</h1>
+    </body>
+</html>
