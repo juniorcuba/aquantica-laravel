7a7a677aae0fa041956c72f0ba10e6e333e8e88f
Roberto Estevez <rjr.eh1996@gmail.com>
2025-06-08 23:05:31 -0500

    loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+window.addEventListener('load', function () {
+    const preloader = document.getElementById('preloader');
+    if (preloader) {
+        preloader.classList.add('opacity-0');
+        setTimeout(() => preloader.style.display = 'none', 300); // Espera la transición
+    }
+});
 import './bootstrap';
