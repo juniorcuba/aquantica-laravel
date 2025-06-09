5c815782dae59bed14d8c88dc7c2c971e898bbb4
Roberto Estevez <rjr.eh1996@gmail.com>
2025-06-08 21:32:11 -0500

    404 design

---
+++ resources/lang/es/not-found.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+  "not_found_title"=> "Página no encontrada",
+  "not_found_description"=> "No pudimos encontrar la página que estabas buscando.",
+  "not_found_cta"=> "Volver al inicio"
+];
